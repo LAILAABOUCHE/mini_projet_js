e3b3abe45333cccea4824388c48fc4f831c14e52
LAILAABOUCHE <lailaabouche02@gmail.com>
2025-03-06 00:09:02 +0000

    updates

---
+++ script.js
@@ -0,0 +1,24 @@
+function Addition(){
+
+    
+}
+function Soustraire(){
+
+
+}
+function Diviser(){
+
+
+}
+function Calculer(){
+
+
+}
+function Supprimer(){
+
+
+}
+function Ajouter(valeur){
+
+
+}
